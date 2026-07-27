21b8f8476d978b32542185cccf8c3ac47f408d3f
LePhuThanh <lpthanh710@gmail.com>
2026-07-27 22:43:04 +0700

    init code-craft-hub-backend project

---
+++ code-craft-hub-backend/src/main/java/com/phelim/code_craft_hub_backend/CodeCraftHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.phelim.code_craft_hub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCraftHubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeCraftHubBackendApplication.class, args);
+	}
+
+}
---
+++ code-craft-hub-backend/src/test/java/com/phelim/code_craft_hub_backend/CodeCraftHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phelim.code_craft_hub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeCraftHubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
